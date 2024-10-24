3d4491b81e291389bce0492e45bf0ce7e617beab
Mohd Dawood <serious17org@gmail.com>
2024-10-24 11:41:10 +0530

    Bug fixes, refactoring and optimizing

---
+++ src/images.js
@@ -0,0 +1,11 @@
+const images = [
+  "https://images.pexels.com/photos/7958188/pexels-photo-7958188.jpeg",
+  "https://images.pexels.com/photos/16631051/pexels-photo-16631051/free-photo-of-close-up-of-green-leaves.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/28871575/pexels-photo-28871575/free-photo-of-scenic-view-of-lush-green-landscape-with-trees.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/15876211/pexels-photo-15876211/free-photo-of-wallpaper-of-fronds.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/28950862/pexels-photo-28950862/free-photo-of-historic-bell-tower-in-dubrovnik-with-flying-birds.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/27467770/pexels-photo-27467770/free-photo-of-laptop-and-camera-on-desk.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/20351329/pexels-photo-20351329/free-photo-of-roses-and-tulips-on-wedding-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
+
+export default images;
